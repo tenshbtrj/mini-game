02c1775ae97c33c809c7646c0c376adbb81834c1
tenshbtrj <55139849+tenshbtrj@users.noreply.github.com>
2025-10-15 19:39:44 +0900

    メタデータ確認用ログを追加

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+console.log(import.meta.env.MODE);  // アプリが動作しているモード
+console.log(import.meta.env.BASE_URL);  // アプリのベースURL
+console.log(import.meta.env.PROD);  // プロダクションモードかどうか
+console.log(import.meta.env.DEV);  // 開発モードかどうか
+console.log(import.meta.env.SSR);  // サーバーサイドレンダリングかどうか
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
